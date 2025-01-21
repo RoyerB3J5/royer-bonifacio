1b55bd2433638aa4d218feb0b4802b4ad202498f
Royer Bonifacio <royer.boni13@gmail.com>
2025-01-20 21:12:41 -0500

    Insert favicon

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -2,7 +2,7 @@ function Email() {
   return (
     <section className=" flex flex-col justify-between items-center gap-10 right-9 bottom-0 z-10 fixed">
       <a
-        href=""
+        href="mailto:royer.boni13@gmail.com"
         className="hover:text-primary-dark hover:dark:text-primary-ligth cursor-pointer hover:-translate-y-1 transition-all "
         style={{ writingMode: "vertical-rl" }}
       >
--- src/components/IconContacts.tsx
+++ src/components/IconContacts.tsx
@@ -5,16 +5,16 @@ import { FiGithub, FiLinkedin } from "react-icons/fi";
 function IconContacts() {
   return (
     <div className="w-full flex flex-row lg:flex-col justify-center items-center gap-5">
-      <a href="https://github.com/RoyerB3J5">
+      <a href="https://github.com/RoyerB3J5" target="_blank" rel="noopener noreferrer">
         <FiGithub
           className="size-5 
               hover:text-primary-dark hover:dark:text-primary-ligth  cursor-pointer hover:-translate-y-1 transition-all"
         />
       </a>
-      <a href="">
+      <a href="" target="_blank" rel="noopener noreferrer">
         <FaInstagram className="size-5 hover:text-primary-dark hover:dark:text-primary-ligth cursor-pointer hover:-translate-y-1 transition-all" />
       </a>
-      <a href="https://www.linkedin.com/in/royer-bonifacio-2984a42a7/">
+      <a href="https://www.linkedin.com/in/royer-bonifacio-2984a42a7/" target="_blank" rel="noopener noreferrer">
         <FiLinkedin className="size-5 hover:text-primary-dark hover:dark:text-primary-ligth cursor-pointer hover:-translate-y-1 transition-all" />
       </a>
     </div>
